a2224bf9d9ca58f0d15275af9cdc9440c65a9c0b
SahebChandok <sahebchandok2001@gmail.com>
2024-08-09 15:53:52 -0400

    UI Changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,8 +21,8 @@ export default function Home() {
         <title>Saheb Chandok</title>
       </Head>
       <main>
-        <section className="text-gray-600 border-b-2 border-gray-200 mx-28 ">
-          <nav className="py-5 px-10 mb-12 flex justify-between border-b-2 border-gray-200 w-full fixed top-0 left-0 bg-gray-50">
+        <section className="text-gray-600 border-b-2 border-gray-200 mx-28">
+          <nav className="py-5 px-10 mb-12 flex justify-between  w-full fixed top-0 left-0 bg-gray-50">
             <h1 className="text-xl text-gray-600">{"{developedbysaheb}"}</h1>
             <ul className="flex flex-col lg:flex-row gap-x-8 items-center text-lg">
               <li><BsFillMoonStarsFill className="cursor-pointer text-xl" /></li>
@@ -35,26 +35,26 @@ export default function Home() {
             </ul>
           </nav>
 
-          <div className="flex justify-between">
-            <div className="flex flex-col px-20 items-center justify-center h-screen">
+          <div className="flex justify-between mt-20">
+            <div className="flex flex-col px-20 items-center justify-center">
               <ul>
-                <li className="pb-10 text-2xl">
+                <li className="pb-10 text-3xl">
                   <Link href="https://www.linkedin.com/in/saheb-singh-chandok/"><FaLinkedinIn /></Link>
                 </li>
-                <li className="pb-10 text-2xl">
+                <li className="pb-10 text-3xl">
                   <Link href="https://github.com/SahebChandok"><FaGithub /></Link>
                 </li>
-                <li className="pb-10 text-2xl">
+                <li className="pb-10 text-3xl">
                   <Link href="https://www.instagram.com/saheb_chandok/"><FaInstagram /></Link>
                 </li>
               </ul>
             </div>
-            <div className="pt-20">
-              <h2 className="flex items-center text-4xl font-extrabold text-gray-700 pt-20">Saheb Chandok <IoMdHand className="text-yellow-500" /></h2>
+            <div className="pt-20 w-[750px]">
+              <h2 className="flex items-center text-6xl font-extrabold text-gray-700 pt-20">Saheb Chandok <IoMdHand className="text-yellow-500" /></h2>
 
 
               <div className="flex items-center py-5 text-gray-500">
-                <PiPenNibStraightLight className="mr-2 font-extrabold" />
+                <PiPenNibStraightLight className="mr-2 font-extrabold text-4xl" />
                 <TypeAnimation
                   sequence={[
                     'Graduate Student',
@@ -63,22 +63,22 @@ export default function Home() {
                     1000,
                     'Software Developer',
                     1000,
-                    'Footballer',
+                    'Football Player',
                     1000,
                   ]}
                   wrapper="span"
                   speed={50}
-                  style={{ fontSize: '2em', display: 'inline-block' }}
+                  style={{ fontSize: '3em', display: 'inline-block' }}
                   repeat={Infinity}
                 />
               </div>
 
-              <p>I am a Software Developer based in Montreal,<br></br> and I am very passionate and dedicated towards my work. </p>
-              <button className="bg-gray-700 text-white flex items-center my-20 rounded-lg px-7 py-5">Say Hello <FaRegPaperPlane className="ml-4" /></button>
-              <h2 className="flex items-center">Scroll Down <FaArrowDownLong /></h2>
+              <p className="text-xl mt-5">I'm a Software Developer with over a year of experience in the IT industry. Currently I am pursuing Masters degree in Applied Computer Science from Concordia University,Canada. Feel free to explore my projects and get in touch.</p>
+              <button className="bg-gray-700 text-white flex items-center my-12 rounded-2xl px-7 py-5">Say Hello <FaRegPaperPlane className="ml-4" /></button>
+              <h2 className="flex items-center mt-28">Scroll Down <FaArrowDownLong /></h2>
             </div>
-            <div className="pt-10 mr-20 content-center">
-              <Image src="/picture.png" alt="Not Found" width={500} height={500} className="" />
+            <div className="content-center">
+              <Image src="/picture.png" alt="Not Found" width={500} height={700} className="w-[580px] h-[750px]" />
             </div>
             
           </div>
